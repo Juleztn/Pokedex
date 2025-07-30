d2291b3e9f53541ef513c053681c0c652a9298f5
Jule Zieten <ju-zie@web.de>
2025-07-30 17:55:37 +0200

    tried to show pokemons type as image, doesnt work yet

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const pokemonRef = document.getElementById('pokemon');
+const pokemonImg = document.getElementsByClassName('pokemon-img');
 
 function init() {
     getPokemonApi();
@@ -9,11 +10,19 @@ async function getPokemonApi() {
         let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}/`);
         let pokemonJson = await pokemon.json();
         console.log(pokemonJson);
-        
-        renderPokemon(pokemonJson, index);
+        renderPokemon(pokemonJson);
     }
 }
 
-function renderPokemon(pokemonJson, i) {
-    pokemonRef.innerHTML += showFirstTwentyPokemon(pokemonJson, i);
+function renderPokemon(pokemonJson) {
+    pokemonRef.innerHTML += showFirstTwentyPokemon(pokemonJson);
+}
+
+async function showPokemonType(pokemonJson) {
+    for (let i = 0; i < pokemonJson.types.length; i++) {
+        let pokemonType = await fetch(`${pokemonJson.types[i].type.url}`);
+        let pokemonTypeJson = await pokemonType.json();
+        let imgSrc = pokemonTypeJson.sprites['generation-viii']['sword-shield'].name_icon;
+        renderPokemonType(imgSrc);
+    }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,17 @@
-function showFirstTwentyPokemon(pokemonJson, i) {
+function showFirstTwentyPokemon(pokemonJson) {
     return `
         <div class="pokemon-card">
-            <h3>${pokemonJson.name}</h3>
-            <img class="pokemon-img" src="${pokemonJson.sprites.front_default}">
+            <h3>${pokemonJson.name.charAt(0).toUpperCase() + pokemonJson.name.slice(1)}</h3>
+            <img class="pokemon-img" src="${pokemonJson.sprites.other['official-artwork'].front_default}">
+            <div>
+                ${showPokemonType(pokemonJson)}
+            </div>
         </div>
+    `;
+}
+
+function renderPokemonType(imgSrc) {
+    return `
+        <img src="${imgSrc}">
     `;
 }
