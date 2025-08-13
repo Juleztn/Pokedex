b012e30c180ae371827bc69691f0ac975d5e4845
Jule Zieten <ju-zie@web.de>
2025-08-13 14:37:06 +0200

    type fetch and evo chain fetch changed, responsive updated, small style changes, updateArrows

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const pokemonRef = document.getElementById('pokemon');
-let pokemonCards = document.getElementsByClassName('pokemon-card');
-const pokemonImg = document.getElementsByClassName('pokemon-img');
 const pokemonTypeRef = document.getElementsByClassName('pokemon-type');
 const pokemonTypeDialogRef = document.getElementsByClassName('pokemon-type-dialog');
 const pokemonAbilitiesRef = document.getElementsByClassName('abilities');
@@ -8,26 +6,34 @@ const loadingScreenRef = document.getElementById('loading-screen');
 const btn = document.getElementById('btn');
 const dialog = document.getElementById('dialog');
 const overlay = document.getElementById('overlay');
-let leftArrow = document.getElementsByClassName('left-arrow');
-let rightArrow = document.getElementsByClassName('right-arrow')
-let input = document.getElementById('search-input');
-let mainInfos = document.getElementsByClassName('main');
-let stats = document.getElementsByClassName('stats');
-let evoChain = document.getElementsByClassName('evo-chain');
-let pokemonDetails = document.getElementsByClassName('pokemon-details');
-let pokemonNameAndImage = document.getElementsByClassName('pokemon-name-and-img');
+const leftArrow = document.getElementsByClassName('left-arrow');
+const rightArrow = document.getElementsByClassName('right-arrow');
+const input = document.getElementById('search-input');
+const mainInfos = document.getElementsByClassName('main');
+const stats = document.getElementsByClassName('stats');
+const evoChain = document.getElementsByClassName('evo-chain');
+const pokemonDetails = document.getElementsByClassName('pokemon-details');
+
 let pokemonArr = [];
 let pokemonNames = [];
-let pokemonFilteredArr = [];
-let pokemonJson;
-let pokemonTypeJson;
-let pokemonTypeJsonDialog;
-let weight;
-let pokemonAbilities;
+let filteredPokemonArr = []; 
+let filteredPokemonNames = [];
+let currentList = []; 
+let currentIndex = 0; 
 let pokemonStartAmount = 1;
 let pokemonAmount = 20;
 let pokemonIndex = 0;
 let isFilter = false;
+let weight;
+
+async function fetchJSON(url) {
+    return (await fetch(url)).json();
+}
+
+function getWeightString(pokemon) {
+    let num = pokemon.weight.toString();
+    return `${num[0]},${num.slice(1)} kg`;
+}
 
 function init() {
     getPokemonApi();
@@ -35,80 +41,94 @@ function init() {
 
 async function getPokemonApi() {
     showLoadingScreen();
-    for (pokemonStartAmount; pokemonStartAmount <= pokemonAmount; pokemonStartAmount++) {
-        let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonStartAmount}/`);
-        pokemonJson = await pokemon.json();
-        pokemonArr.push(pokemonJson);
-        pokemonNames.push(pokemonJson.name);
-    }
-    console.log(pokemonArr);
+    let allPokemon = await loadPokemonRange(pokemonStartAmount, pokemonAmount);
+    pokemonArr = await Promise.all(allPokemon.map(enrichPokemonData));
+    pokemonNames = pokemonArr.map(p => p.name);
     hideLoadingScreen();
     renderPokemon();
 }
 
-async function renderPokemon() {
-    for (pokemonIndex; pokemonIndex < pokemonArr.length; pokemonIndex++) {
-        pokemonRef.innerHTML += showPokemon(pokemonArr, pokemonIndex);
-        showPokemonType(pokemonIndex);
+async function loadPokemonRange(start, end) {
+    let promises = [];
+    for (let i = start; i <= end; i++) {
+        promises.push(fetchJSON(`https://pokeapi.co/api/v2/pokemon/${i}/`));
     }
+    return Promise.all(promises);
 }
 
-async function showPokemonType(index) {
-    for (let i = 0; i < pokemonArr[index].types.length; i++) {
-        let pokemonType = await fetch(`${pokemonArr[index].types[i].type.url}`);
-        pokemonTypeJson = await pokemonType.json();
-        let imgSrc = await pokemonTypeJson.sprites['generation-viii']['sword-shield'].name_icon;
-        pokemonTypeRef[index].innerHTML += renderPokemonType(imgSrc);
+async function enrichPokemonData(p) {
+    p.typeIcons = await loadTypeIcons(p.types);
+    p.evolutionChain = await loadEvolutionChain(p.species.url);
+    return p;
+}
+
+async function loadTypeIcons(types) {
+    return Promise.all(types.map(async t => {
+        let typeData = await fetchJSON(t.type.url);
+        return typeData.sprites['generation-viii']['sword-shield'].name_icon;
+    }));
+}
+
+async function loadEvolutionChain(speciesUrl) {
+    let speciesData = await fetchJSON(speciesUrl);
+    let evoData = await fetchJSON(speciesData.evolution_chain.url);
+    let chain = [];
+    let current = evoData.chain;
+    while (current) {
+        let evoPokeData = await fetchJSON(`https://pokeapi.co/api/v2/pokemon/${current.species.name}/`);
+        chain.push(evoPokeData);
+        current = current.evolves_to[0] || null;
+    }
+    return chain;
+}
+
+function renderPokemon() {
+    for (; pokemonIndex < pokemonArr.length; pokemonIndex++) {
+        pokemonRef.innerHTML += showPokemon(pokemonArr, pokemonIndex);
     }
 }
 
 async function showMorePokemon() {
-    pokemonAmount = pokemonAmount + 20;
-    getPokemonApi();
+    showLoadingScreen();
+    const oldAmount = pokemonAmount;
+    pokemonAmount += 20;
+    let newPokemon = await loadPokemonRange(oldAmount + 1, pokemonAmount);
+    let enriched = await Promise.all(newPokemon.map(enrichPokemonData));
+    pokemonArr = pokemonArr.concat(enriched);
+    if (!isFilter) currentList = pokemonArr;
+    hideLoadingScreen();
+    enriched.forEach((p, idx) => {
+        const i = oldAmount + idx;
+        pokemonRef.innerHTML += showPokemon(pokemonArr, i);
+    });
 }
 
 function showLoadingScreen() {
     loadingScreenRef.classList.remove('d_none');
     btn.classList.add('d_none');
 }
-
 function hideLoadingScreen() {
     loadingScreenRef.classList.add('d_none');
     btn.classList.remove('d_none');
 }
 
 function filterPokemon() {
-    isFilter = true;
+    let term = input.value.trim().toUpperCase();
     let alert = document.getElementById('alert');
-    if (input.value.length >= 3) {
-        alert.classList.add('d_none');
-        filterAndShowPokemon();
-    } else if (input.value == "") {
-        alert.classList.add('d_none');
-        showAllPokemon();
-    } else if (input.value.length > 0 && input.value.length < 3) {
-        alert.classList.remove('d_none');
-        isFilter = false;
-    }
+    alert.classList.toggle('d_none', term.length === 0 || term.length >= 3);
+    if (term.length < 3 && term.length > 0) return;
+    isFilter = term.length >= 3;
+    let filtered = pokemonArr.filter(p => p.name.toUpperCase().includes(term));
+    currentList = filtered.length > 0 ? filtered : pokemonArr;
+    renderFilteredPokemon(currentList);
 }
 
-function filterAndShowPokemon() {
-    pokemonFilteredArr = [];
-    let filter, a, txtValue;
-    filter = input.value.toUpperCase();
-    for (let i = 0; i < pokemonNames.length; i++) {
-        a = pokemonCards[i].getElementsByTagName("h3")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            pokemonCards[i].style.display = "";
-            pokemonFilteredArr.push(pokemonCards[i]);
-            console.log(pokemonFilteredArr);
-        } else {
-            pokemonCards[i].style.display = "none";
-            isFilter = false;
-        }
-    }
-    btn.classList.add('d_none');
+function renderFilteredPokemon(list) {
+    pokemonRef.innerHTML = "";
+    list.forEach((p, i) => {
+        pokemonRef.innerHTML += showFilteredPokemon(p, i);
+    });
+    btn.classList.toggle('d_none', isFilter);
 }
 
 function showAllPokemon() {
@@ -119,40 +139,91 @@ function showAllPokemon() {
     renderPokemon();
 }
 
-function openPokemonDialog(i) {
-    dialog.show();
-    getWeightOfPokemon(i);
+function fillDialog(i) {
+    let p = pokemonArr[i];
+    weight = getWeightString(p);
     dialog.innerHTML = showClickedPokemon(i, weight);
+    pokemonAbilitiesRef[0].innerHTML = p.abilities.map(a => a.ability.name).join(', ');
     if (i == 0) {
-        leftArrow[0].innerHTML = "";
-    }
-    if (i == pokemonAmount - 1) {
-        rightArrow[0].innerHTML = "";
-    }
-    overlay.classList.toggle('d_none');
-    getPokemonTypeForDialog(i);
-    getAbilityofPokemon(i);
-    getPokemonEvolutionChain(i);
-
-    if (!isFilter) {
-        console.log('test');
-        
+        leftArrow[0].innerHTML = '';
     }
 }
 
-function getWeightOfPokemon(i) {
-    let number = pokemonArr[i].weight.toString();
-    weight = `${number[0]},${number.slice(1)} kg`;
+function openPokemonDialog(i) {
+    if (!currentList || currentList.length === 0) currentList = pokemonArr;
+    currentIndex = i;
+    fillDialogFromList(currentList, currentIndex);
+    dialog.show();
+    overlay.classList.remove('d_none');
+    updateArrows(); // Pfeile direkt beim Öffnen updaten
 }
 
-function getAbilityofPokemon(i) {
-    for (let index = 0; index < pokemonArr[i].abilities.length; index++) {
-        pokemonAbilities = pokemonArr[i].abilities[index].ability.name;
-        if (index < pokemonArr[i].abilities.length - 1) {
-            pokemonAbilitiesRef[0].innerHTML += `${pokemonAbilities},` + '\xa0\xa0\xa0\xa0';
-        } else {
-            pokemonAbilitiesRef[0].innerHTML += `${pokemonAbilities}`;
-        }
+function renderTypeIconsDialog(typeIcons) {
+    return typeIcons.map(src => `
+        <img src="${src}">
+    `).join('');
+}
+
+function updateDialogContent(list, i) {
+    const p = list[i];
+    weight = getWeightString(p);
+    dialog.innerHTML = showClickedPokemonFromList(list, i, weight);
+    pokemonAbilitiesRef[0].innerHTML = p.abilities.map(a => a.ability.name).join(', ');
+}
+
+function fillDialogFromList(list, index) {
+    let p = list[index];
+    weight = getWeightString(p);
+    dialog.innerHTML = showClickedPokemonFromList(list, index, weight);
+    pokemonAbilitiesRef[0].innerHTML = p.abilities.map(a => a.ability.name).join(', ');
+}
+
+function previousPokemon() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        fillDialogFromList(currentList, currentIndex);
+        updateArrows();
+    }
+}
+
+function nextPokemon() {
+    if (currentIndex < currentList.length - 1) {
+        currentIndex++;
+        fillDialogFromList(currentList, currentIndex);
+        updateArrows();
+    }
+}
+function previousPokemonFromList() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        let p = currentList[currentIndex];
+        weight = getWeightString(p);
+        dialog.innerHTML = showClickedPokemonFromList(currentList, currentIndex, weight);
+        pokemonAbilitiesRef[0].innerHTML = p.abilities.map(a => a.ability.name).join(', ');
+    }
+}
+
+function nextPokemonFromList() {
+    if (currentIndex < currentList.length - 1) {
+        currentIndex++;
+        let p = currentList[currentIndex];
+        weight = getWeightString(p);
+        dialog.innerHTML = showClickedPokemonFromList(currentList, currentIndex, weight);
+        pokemonAbilitiesRef[0].innerHTML = p.abilities.map(a => a.ability.name).join(', ');
+    }
+}
+
+function updateArrows() {
+    leftArrow[0].style.display = currentIndex > 0 ? "block" : "none";
+
+    if (currentIndex < currentList.length - 1) {
+        rightArrow[0].style.display = "flex";           // immer flex, wenn sichtbar
+        rightArrow[0].style.justifyContent = "flex-end"; // rechts ausrichten
+        rightArrow[0].style.width = "100%";            // volle Breite
+    } else {
+        rightArrow[0].style.display = "none";          // ausblenden
+        rightArrow[0].style.justifyContent = "";       // zurücksetzen
+        rightArrow[0].style.width = "";                // zurücksetzen
     }
 }
 
@@ -161,21 +232,12 @@ function toggleOverlay() {
     dialog.close();
 }
 
-async function getPokemonTypeForDialog(index) {
-    for (let i = 0; i < pokemonArr[index].types.length; i++) {
-        let pokemonTypeDialog = await fetch(`${pokemonArr[index].types[i].type.url}`);
-        pokemonTypeJsonDialog = await pokemonTypeDialog.json();
-        let imgSrcDialog = await pokemonTypeJsonDialog.sprites['generation-viii']['sword-shield'].name_icon;
-        pokemonTypeDialogRef[0].innerHTML += showPokemonTypeDialog(imgSrcDialog);
-    }
-}
-
 function showMainInfos(i) {
     mainInfos[0].classList.add('underline');
     stats[0].classList.remove('underline');
     evoChain[0].classList.remove('underline');
     pokemonDetails[0].innerHTML = showMainInfosHtml(i);
-    getAbilityofPokemon(i);
+    pokemonAbilitiesRef[0].innerHTML = pokemonArr[i].abilities.map(a => a.ability.name).join(', ');
 }
 
 function showStats(i) {
@@ -189,60 +251,9 @@ function showEvoChain(i) {
     evoChain[0].classList.add('underline');
     stats[0].classList.remove('underline');
     mainInfos[0].classList.remove('underline');
-
-    const evolutionChain = pokemonArr[i].evolutionChain;
-
-    switch (evolutionChain.length) {
-        case 1:
-            pokemonDetails[0].innerHTML = showEvoChainWithOnePokemon(evolutionChain[0]);
-            break;
-        case 2:
-            pokemonDetails[0].innerHTML = showEvoChainWithTwoPokemon(evolutionChain[0], evolutionChain[1]);
-            break;
-        case 3:
-            pokemonDetails[0].innerHTML = showEvoChainWithThreePokemon(evolutionChain[0], evolutionChain[1], evolutionChain[2]);
-            break;
-    }
-}
-
-async function getPokemonEvolutionChain(i) {
-    let pokemonSpecies = await fetch(pokemonArr[i].species.url);
-    let pokemonSpeciesJson = await pokemonSpecies.json();
-    let pokemonEvo = await fetch(pokemonSpeciesJson.evolution_chain.url);
-    let pokemonEvoJson = await pokemonEvo.json();
-
-    let evolutionChain = [];
-    let currentEvolution = pokemonEvoJson.chain;
-
-    while (currentEvolution) {
-        let pokemonName = currentEvolution.species.name;
-        let pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
-        let pokemonJson = await pokemonData.json();
-        evolutionChain.push(pokemonJson);
-
-        if (currentEvolution.evolves_to.length > 0) {
-            currentEvolution = currentEvolution.evolves_to[0];
-        } else {
-            break;
-        }
-    }
-    pokemonArr[i].evolutionChain = evolutionChain;
-}
-
-function previousPokemon(i) {
-    if (i > 0) {
-        i--;
-        dialog.innerHTML = showClickedPokemon(i, weight);
-        getPokemonTypeForDialog(i);
-        getAbilityofPokemon(i);
-    }
-}
-
-function nextPokemon(i) {
-    if (i < pokemonAmount - 1) {
-        i++;
-        dialog.innerHTML = showClickedPokemon(i, weight);
-        getPokemonTypeForDialog(i);
-        getAbilityofPokemon(i);
-    }
+    const list = currentList.length > 0 ? currentList : pokemonArr;
+    const chain = list[i].evolutionChain;
+    if (chain.length === 1) pokemonDetails[0].innerHTML = showEvoChainWithOnePokemon(chain[0]);
+    if (chain.length === 2) pokemonDetails[0].innerHTML = showEvoChainWithTwoPokemon(chain[0], chain[1]);
+    if (chain.length === 3) pokemonDetails[0].innerHTML = showEvoChainWithThreePokemon(chain[0], chain[1], chain[2]);
 }
--- scripts/template.js
+++ scripts/template.js
@@ -3,30 +3,36 @@ function showPokemon(pokemonArr, i) {
         <div onclick="openPokemonDialog(${i})" class="pokemon-card ${pokemonArr[i].types[0].type.name}">
             <h3>${pokemonArr[i].name.charAt(0).toUpperCase() + pokemonArr[i].name.slice(1)}</h3>
             <div class="pokemon-img-type">
-                <img class="pokemon-img" src="${pokemonArr[i].sprites.other['official-artwork'].front_default}">
-                <div class="pokemon-type"></div>
+                <img class="pokemon-img pokemon-card-img" src="${pokemonArr[i].sprites.other['official-artwork'].front_default}">
+                <div class="pokemon-type">
+                    ${renderTypeIcons(pokemonArr[i].typeIcons)}
+                </div>
             </div>
         </div>
     `;
 }
 
-function renderPokemonType(imgSrc) {
-    return `
+function renderTypeIcons(typeIcons) {
+    return typeIcons.map(src => `
         <div class="img-wrapper">
-            <img class="type-img" src=${imgSrc}>
+            <img class="type-img" src="${src}">
         </div>
-    `;
+    `).join('');
 }
 
 function showClickedPokemon(i, weight) {
     return `
         <div class="pokemon-dialog ${pokemonArr[i].types[0].type.name}">
-        <div onclick="toggleOverlay()" class="x-mark-container ${pokemonArr[i].types[0].type.name}-hover"><img class="x-mark" src="./img/xmark-white.svg"></div>
+            <div onclick="toggleOverlay()" class="x-mark-container ${pokemonArr[i].types[0].type.name}-hover">
+                <img class="x-mark" src="./img/xmark-white.svg">
+            </div>
             <div class="pokemon-name-and-img">
                 <h2>${pokemonArr[i].name.charAt(0).toUpperCase() + pokemonArr[i].name.slice(1)}</h2>
                 <img class="pokemon-img img-dialog" src="${pokemonArr[i].sprites.other['official-artwork'].front_default}">
             </div>
-            <div class="pokemon-type-dialog"></div>
+            <div class="pokemon-type-dialog">
+                ${renderTypeIconsDialog(pokemonArr[i].typeIcons)}
+            </div>
             <div class="pokemon-info ${pokemonArr[i].types[0].type.name}-info">
                 <span onclick="showMainInfos(${i})" class="info main underline">main</span>
                 <span onclick="showStats(${i})" class="info stats">stats</span>
@@ -34,34 +40,74 @@ function showClickedPokemon(i, weight) {
             </div>
             <div class="pokemon-details">
                 <div class="main-info">
-                    <div class="single-info">
-                        <span>Height:</span>
-                        <span>${pokemonArr[i].height}0 cm</span>
-                    </div>
-                    <div class="single-info">
-                        <span>Weight:</span>
-                        <span>${weight}</span>
-                    </div>
-                    <div class="single-info">
-                        <span>Base Experience:</span>
-                        <span>${pokemonArr[i].base_experience}</span>
-                    </div>
-                    <div class="single-info">
-                        <span>Abilities:</span>
-                        <div class="abilities"></div>
-                    </div>
+                    <div class="single-info"><span>Height:</span><span>${pokemonArr[i].height}0 cm</span></div>
+                    <div class="single-info"><span>Weight:</span><span>${weight}</span></div>
+                    <div class="single-info"><span>Base Experience:</span><span>${pokemonArr[i].base_experience}</span></div>
+                    <div class="single-info"><span>Abilities:</span><div class="abilities"></div></div>
                 </div>
             </div>
             <div class="arrows">
                 <div class="left-arrow">
-                    <img onclick="previousPokemon(${i})" class="arrows-img ${pokemonArr[i].types[0].type.name}-hover" src="./img/angle-left-white.svg">
+                    <img onclick="previousPokemon()" class="arrows-img ${pokemonArr[i].types[0].type.name}-hover" src="./img/angle-left-white.svg">
                 </div>
                 <div class="right-arrow">
-                    <img onclick="nextPokemon(${i})" class="arrows-img ${pokemonArr[i].types[0].type.name}-hover" src="./img/angle-right-white.svg">
+                    <img onclick="nextPokemon()" class="arrows-img ${pokemonArr[i].types[0].type.name}-hover" src="./img/angle-right-white.svg">
                 </div>
             </div>
         </div>
     `;
+}
+
+function showClickedPokemonFromList(list, i, weight) {
+    return `
+        <div class="pokemon-dialog ${list[i].types[0].type.name}">
+            <div onclick="toggleOverlay()" class="x-mark-container ${list[i].types[0].type.name}-hover">
+                <img class="x-mark" src="./img/xmark-white.svg">
+            </div>
+            <div class="pokemon-name-and-img">
+                <h2>${list[i].name.charAt(0).toUpperCase() + list[i].name.slice(1)}</h2>
+                <img class="pokemon-img img-dialog" src="${list[i].sprites.other['official-artwork'].front_default}">
+            </div>
+            <div class="pokemon-type-dialog">
+                ${renderTypeIconsDialog(list[i].typeIcons)}
+            </div>
+            <div class="pokemon-info ${list[i].types[0].type.name}-info">
+                <span onclick="showMainInfos(${i})" class="info main underline">main</span>
+                <span onclick="showStats(${i})" class="info stats">stats</span>
+                <span onclick="showEvoChain(${i})" class="info evo-chain">evo chain</span>
+            </div>
+            <div class="pokemon-details">
+                <div class="main-info">
+                    <div class="single-info"><span>Height:</span><span>${list[i].height}0 cm</span></div>
+                    <div class="single-info"><span>Weight:</span><span>${weight}</span></div>
+                    <div class="single-info"><span>Base Experience:</span><span>${list[i].base_experience}</span></div>
+                    <div class="single-info"><span>Abilities:</span><div class="abilities"></div></div>
+                </div>
+            </div>
+            <div class="arrows">
+                <div class="left-arrow">
+                    <img onclick="previousPokemon(${i})" class="arrows-img ${list[i].types[0].type.name}-hover" src="./img/angle-left-white.svg">
+                </div>
+                <div class="right-arrow">
+                    <img onclick="nextPokemon(${i})" class="arrows-img ${list[i].types[0].type.name}-hover" src="./img/angle-right-white.svg">
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function showFilteredPokemon(p, i) {
+    return `
+            <div onclick="openPokemonDialog(${i})" class="pokemon-card ${p.types[0].type.name}">
+                <h3>${p.name.charAt(0).toUpperCase() + p.name.slice(1)}</h3>
+                <div class="pokemon-img-type">
+                    <img class="pokemon-img pokemon-card-img" src="${p.sprites.other['official-artwork'].front_default}">
+                    <div class="pokemon-type">
+                        ${renderTypeIcons(p.typeIcons)}
+                    </div>
+                </div>
+            </div>
+        `;
 }
 
 function changeNameAndImage(i) {
